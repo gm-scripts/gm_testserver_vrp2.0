87a28cac714e1e2926c9003d5612a9c16461c347
Eichenholz <62652381+Eichenholz@users.noreply.github.com>
2021-06-15 21:42:39 +0200

    Initial commit

---
+++ resources/resources/gm_npc/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Eichenholz'
+description 'NPC Control / GM-Scripts'
+
+client_scripts {
+    'client/main.js'
+}
+
+server_scripts {
+    'server/main.js'
+}
